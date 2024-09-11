ab47e2b26f1fbde3602cd217d2b7d9634016f7cc
caojunhui <caojunhui.sticey@bytedance.com>
2024-09-11 11:16:32 +0800

    fix log id can't be null

--- fe/fe-core/src/main/java/org/apache/doris/qe/StmtExecutor.java
+++ fe/fe-core/src/main/java/org/apache/doris/qe/StmtExecutor.java
@@ -1048,7 +1048,7 @@ public class StmtExecutor {
             optHints = setOperationStmt.collectHints();
         }
 
-        if (!optHints.isEmpty()) {
+        if (optHints != null && !optHints.isEmpty()) {
             sessionVariable.setIsSingleSetVar(true);
             for (String key : optHints.keySet()) {
                 if (key.equals("log_id")) {
