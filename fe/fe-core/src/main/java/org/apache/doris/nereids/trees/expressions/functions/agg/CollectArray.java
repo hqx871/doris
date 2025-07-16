c8b9ceefb5d9bceb6f6bd5ea44b845942c930197
huanghui.bigrey <huanghui.bigrey@bytedance.com>
2025-07-16 15:16:06 +0800

    fix checkstyle

--- fe/fe-core/src/main/java/org/apache/doris/nereids/trees/expressions/functions/agg/CollectArray.java
+++ fe/fe-core/src/main/java/org/apache/doris/nereids/trees/expressions/functions/agg/CollectArray.java
@@ -24,7 +24,6 @@ import org.apache.doris.nereids.trees.expressions.functions.ExplicitlyCastableSi
 import org.apache.doris.nereids.trees.expressions.shape.UnaryExpression;
 import org.apache.doris.nereids.types.ArrayType;
 import org.apache.doris.nereids.types.coercion.AnyDataType;
-import org.apache.doris.nereids.types.coercion.FollowToAnyDataType;
 
 import com.google.common.base.Preconditions;
 import com.google.common.collect.ImmutableList;
